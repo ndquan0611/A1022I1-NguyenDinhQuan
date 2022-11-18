c0dfb6242fb75d1ba05a79af37ef4335b7b45200
dinhquan0611 <nguyendinhquan78910@gmail.com>
2022-11-18 18:06:30 +0700

    add

---
+++ ss7_bien_kieu_du_lieu_va_toan_tu/bai_tap/may_tinh/main.js
@@ -0,0 +1,35 @@
+const result = document.querySelector('.result span');
+const btnElement = document.querySelectorAll('.form-card button');
+
+btnElement.forEach((item) => {
+    item.addEventListener('click', function (e) {
+        const inputElement1 = parseInt(document.querySelector('#form-input1').value);
+        const inputElement2 = parseInt(document.querySelector('#form-input2').value);
+        let sum = 0,
+            sub = 0,
+            mul = 0,
+            div = 0;
+
+        switch (e.target.innerText) {
+            case 'Addition(+)':
+                sum = inputElement1 + inputElement2;
+                result.innerText = sum;
+                break;
+            case 'Subtraction(-)':
+                sub = inputElement1 - inputElement2;
+                result.innerText = sub;
+                break;
+            case 'Multiplication(X)':
+                mul = inputElement1 * inputElement2;
+                result.innerText = mul;
+                break;
+            case 'Division(/)':
+                div = inputElement1 / inputElement2;
+                result.innerText = div;
+                break;
+            default:
+                console.log('Error!!');
+        }
+        e.preventDefault();
+    });
+});
